5a8a7fb79043476aaa9b107d64b0a06cc4b87ef2
justintheyang justintheyang <justintheyang@gmail.com>
2021-05-30 12:37:28 -0700

    prettify download script and ignore experiment metadata.js files

--- download_data.py
+++ download_data.py
@@ -38,4 +38,5 @@ if __name__ == "__main__":
     if overwrite == True or os.path.exists(s3_object.key)==False:
       progressBar(s3_object.key, i+1, len(keys))
       b.download_file(s3_object.key, s3_object.key)
+  print('All done!')
   print()
