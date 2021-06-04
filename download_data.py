036bdea669850b83121ebd9d48b8688675dd6324
justintheyang justintheyang <justintheyang@gmail.com>
2021-06-03 23:25:28 -0700

    add overwrite

--- download_data.py
+++ download_data.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import argparse
 
 '''
 To download data, use command: python download_data.py in the main project directory
@@ -12,7 +13,6 @@ def make_dir_if_not_exists(dir_name):
         os.makedirs(dir_name)
     return dir_name
 
-
 def progressBar(key, current, total, barLength=20):
     percent = float(current) * 100 / total
     arrow   = '-' * int(percent/100 * barLength - 1) + '>'
@@ -20,10 +20,15 @@ def progressBar(key, current, total, barLength=20):
 
     print('Progress: [%s%s] %d %% | uploading %s' % (arrow, spaces, percent, os.path.split(key)[1]), end='\x1b\r')
 
-
-overwrite = False
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1", 'y')
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--overwrite', type=str2bool, help='if True, will overwrite local with download from S3',
+                      default='False')
+  args = parser.parse_args()
+
   s3 = boto3.resource('s3')
   b = s3.Bucket('photodraw-public')
   keys = [obj.key for obj in b.objects.all()]  
@@ -35,7 +40,7 @@ if __name__ == "__main__":
 
   print('Initiating download from S3 ...')
   for i, s3_object in enumerate(b.objects.all()):
-    if overwrite == True or os.path.exists(s3_object.key)==False:
+    if args.overwrite == True or os.path.exists(s3_object.key)==False:
       progressBar(s3_object.key, i+1, len(keys))
       b.download_file(s3_object.key, s3_object.key)
   print()
